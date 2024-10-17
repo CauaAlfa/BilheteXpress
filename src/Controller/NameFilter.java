219d30d4263e50f97f640f5f63814928822623ce
Caua Muaga <muagacaua@gmail.com>
2024-10-17 18:56:31 +0200

    add

--- src/Controller/NameFilter.java
+++ src/Controller/NameFilter.java
@@ -4,10 +4,32 @@
  */
 package Controller;
 
+import javax.swing.text.AttributeSet;
+import javax.swing.text.BadLocationException;
+import javax.swing.text.DocumentFilter;
+
 /**
  *
  * @author Caua
  */
-public class NameFilter {
-    
+public class NameFilter extends DocumentFilter {
+    @Override
+    public void insertString(FilterBypass fb, int offset, String string, AttributeSet attr) throws BadLocationException {
+        if (string == null) {
+            return;
+        }
+        if (!string.matches(".*\\d.*")) { // Verifica se a string contém números
+            super.insertString(fb, offset, string, attr);
+        }
+    }
+
+    @Override
+    public void replace(FilterBypass fb, int offset, int length, String text, AttributeSet attrs) throws BadLocationException {
+        if (text == null) {
+            return;
+        }
+        if (!text.matches(".*\\d.*")) { // Verifica se a string contém números
+            super.replace(fb, offset, length, text, attrs);
+        }
+    }
 }
